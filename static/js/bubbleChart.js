9ef5de90015d290be41cce76aa795ee9c59b92a4
sumeer2602 <sumeerahmed123@gmail.com>
2024-05-08 02:01:05 -0400

    added treemap, bubble chart, and skeleton pcp

---
+++ static/js/bubbleChart.js
@@ -0,0 +1,51 @@
+async function fetchBubbleData() {
+    const response = await fetch("/bubblechart");
+    const data = await response.json();
+    return data;
+}
+
+
+export async function createBubbleChart() {
+    const data = await fetchBubbleData();
+    const width = 700; // Width of the SVG
+    const height = 500; // Height of the SVG
+
+    const svg = d3.select('#bubbleChart').append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    // Sort data by count and slice to get top 10 for labeling
+    data.sort((a, b) => b.count - a.count);
+    const top10Genres = new Set(data.slice(0, 10).map(d => d.genre));
+
+    const pack = d3.pack()
+        .size([width, height])
+        .padding(2);
+
+    const root = d3.hierarchy({children: data})
+        .sum(d => d.count); // Define the value to determine the area of the bubble
+
+    const bubbles = pack(root).leaves();
+
+    const color = d3.scaleOrdinal(d3.schemeCategory10); // Color scale
+
+    svg.selectAll('circle')
+        .data(bubbles)
+        .enter().append('circle')
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y)
+        .attr('r', d => d.r)
+        .style('fill', d => color(d.data.genre));
+
+    // Append text only to the top 10 largest bubbles
+    svg.selectAll('text')
+        .data(bubbles.filter(d => top10Genres.has(d.data.genre)))
+        .enter().append('text')
+        .attr('x', d => d.x)
+        .attr('y', d => d.y)
+        .text(d => d.data.genre)
+        .attr('dy', '.3em')
+        .attr('text-anchor', 'middle')
+        .style('fill', 'white')
+        .style('font-size', '12px');
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,12 @@
 import { createKeysPieChart } from './keysPieChart.js'
 import { createTop30BarChart } from './top30tracks.js'
+import { createTreemap } from './treemap.js'
+import { createBubbleChart } from './bubbleChart.js'
+import { createParallelCoordinatesPlot } from './pcp.js'
 
 createKeysPieChart()
 createTop30BarChart()
+createTreemap()
+createBubbleChart()
+// getData('playlists')
+createParallelCoordinatesPlot('playlist')
