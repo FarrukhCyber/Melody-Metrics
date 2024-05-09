8853a79847c94dcec1c7af4aac4a66744e240c62
Farrukh <muhammadfarrukh2001@gmail.com>
2024-05-09 18:08:46 -0400

    added y-axis label to barPlot

--- static/js/top30tracks.js
+++ static/js/top30tracks.js
@@ -23,7 +23,7 @@ export async function createTop30BarChart(columnName=NaN, filters=NaN) {
 
 
     // const margin = { top: 50, right: 30, bottom: 120, left: 100 },
-    const margin = { top: 10, right: 10, bottom: 120, left: 80 },
+    const margin = { top: 10, right: 10, bottom: 120, left: 120 },
         width = 700 - margin.left - margin.right,
         height = 300 - margin.top - margin.bottom;
 
@@ -60,6 +60,16 @@ export async function createTop30BarChart(columnName=NaN, filters=NaN) {
         .style("fill", "orange");  // Change label color here
 
     svg.selectAll(".tick line").style("stroke", "orange");  // Change axis tick color here
+
+        // Add Y axis label
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -5 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .style("fill", "orange")
+        .text("Music Streams");
 
     // Bars
     svg.selectAll("myBar")
@@ -109,55 +119,5 @@ export async function createTop30BarChart(columnName=NaN, filters=NaN) {
 }
 
     
-// export async function createTop30BarChart() {
-//     const data = await fetchTop30();
-//     const margin = { top: 20, right: 30, bottom: 30, left: 150 },  // Adjust left margin for track names
-//         width = 750 - margin.left - margin.right,
-//         height = 300 - margin.top - margin.bottom;  // Adjusted for clarity with more bars
-
-//     // Append the svg object to the body of the page
-//     const svg = d3.select("#barChart").append("svg")
-//         .attr("width", width + margin.left + margin.right)
-//         .attr("height", height + margin.top + margin.bottom)
-//         .append("g")
-//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-//     // Y axis: Scale and draw:
-//     const y = d3.scaleBand()
-//         .range([0, height])
-//         .domain(data.map(d => d.track_name))
-//         .padding(0.1);
-//     svg.append("g")
-//         .call(d3.axisLeft(y));
-
-//     // X axis: Scale and draw:
-//     const x = d3.scaleLinear()
-//         .domain([0, d3.max(data, d => d.streams)])
-//         .range([0, width]);
-//     svg.append("g")
-//         .attr("transform", "translate(0," + height + ")")
-//         .call(d3.axisBottom(x));
-
-//     // Bars
-//     svg.selectAll("myBar")
-//         .data(data)
-//         .enter()
-//         .append("rect")
-//         .attr("y", d => y(d.track_name))
-//         .attr("x", 0)
-//         .attr("width", d => x(d.streams))
-//         .attr("height", y.bandwidth())
-//         .attr("fill", "#1DB954");
-
-//     // Optional: Add title to the chart
-//     // svg.append("text")
-//     //     .attr("x", width / 2)
-//     //     .attr("y", -10)  // Position above the top margin
-//     //     .attr("text-anchor", "middle")
-//     //     .style("font-size", "20px")
-//     //     .style("font-family", "Arial")
-//     //     .style("fill", "black")
-//     //     .text("Top 30 Spotify Songs Based on Streams");
-// }
 
 
